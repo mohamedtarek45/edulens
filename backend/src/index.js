557e9cbeb3d1329b92f8f0ad11f2835dbef197e1
mohamedtarek45 <moka76615@gmail.com>
2026-04-13 19:42:27 +0200

    test

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ mongoose
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173","http://192.168.1.3:5173"],
+    origin: ["http://localhost:5173","http://192.168.1.3:5173" , process.env.FRONTEND_URL],
     credentials: true,
   }),
 );
